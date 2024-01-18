bcb584bfc72de8d234702150993b74ab1f6189f3
Ahn Jin Sung <48504392+blackhoal@users.noreply.github.com>
2024-01-18 23:34:17 +0900

    240118 TIL

--- TIL/week_4/240118/note.md
+++ TIL/week_4/240118/note.md
@@ -1,3 +1,4 @@
 # async / await
+- 비동기를 동기화시키기 위한 문법
 
 # REST API
--- TIL/week_4/240118/theory/scripts/forecast.js
+++ TIL/week_4/240118/theory/scripts/forecast.js
@@ -1,1 +1,33 @@
 const key = 'fo8caZ7mk98eOAqEQF8goVe8DA08wqTX';
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    // ?apikey = ${ key }& q=${ }
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json()
+
+    // Key:"226081"
+    // console.log(data[0].Key);
+    return data[0];
+}
+
+
+getCity('manchester');
+
+// key
+// :
+// '226081'
+
+// http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${}
